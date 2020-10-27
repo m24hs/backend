9117be27c26eb39b61f6538aead3716c5b577f43
Marcelo Rossini <mar.18.celo@gmail.com>
2020-10-27 12:00:26 -0300

    upload

--- app/server.js
+++ app/server.js
@@ -1,27 +1,14 @@
-require("dotenv/config");
-const express = require("express");
-const routes = require("./routes");
-const cors = require("cors");
+require('dotenv/config');
+const express = require('express');
+const routes = require('./routes');
+const cors = require('cors');
 const server = express();
 
 // Config
-// Add headers
-server.use(function (req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type"
-  );
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  next();
-});
-server.use(express.json({ limit: 1024 * 1024 * 20, type: "application/json" })); 
+server.use(cors());
+server.use(express.json({limit:1024*1024*20, type:'application/json'}));
 server.use(routes);
-server.use("/uploads", express.static(process.cwd() + "/uploads"));
+server.use('/uploads', express.static(process.cwd() + '/uploads'))
 
 // Start
 server.listen(process.env.PORT || 3333);
